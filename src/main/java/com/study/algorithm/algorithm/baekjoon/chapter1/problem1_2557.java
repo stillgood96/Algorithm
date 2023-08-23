5a56992e3ad5300d4f300b768e121655db78e7d9
stillgood96 <a787574@naver.com>
2023-08-23 22:32:44 +0900

    Initial commit

---
+++ src/main/java/com/study/algorithm/algorithm/baekjoon/chapter1/problem1_2557.java
@@ -0,0 +1,14 @@
+package com.study.algorithm.algorithm.baekjoon.chapter1;
+
+public class problem1_2557 {
+
+    /**
+     * Hello World!를 출력하시오.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
