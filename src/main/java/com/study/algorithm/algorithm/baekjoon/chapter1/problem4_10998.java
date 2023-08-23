45df6475d2c68e774f318ff0772bcbf567f42651
stillgood96 <a787574@naver.com>
2023-08-23 22:54:29 +0900

    단계별풀기 입출력과 사칙연산 4-10998 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.

--- src/main/java/com/study/algorithm/algorithm/baekjoon/chapter1/problem4_10998.java
+++ src/main/java/com/study/algorithm/algorithm/baekjoon/chapter1/problem4_10998.java
@@ -2,10 +2,10 @@ package com.study.algorithm.algorithm.baekjoon.chapter1;
 
 import java.util.Scanner;
 
-public class problem3_1001 {
+public class problem4_10998 {
 
     /**
-     * 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+     * 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
      */
     public static void main(String[] args) {
 
@@ -14,9 +14,9 @@ public class problem3_1001 {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int minus = a - b;
+        int multiply = a - b;
 
-        System.out.println(minus);
+        System.out.println(multiply);
 
         sc.close();
 
