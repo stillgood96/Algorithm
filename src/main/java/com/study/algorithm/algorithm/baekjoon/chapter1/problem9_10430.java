e2d3b51759c8fc2a6168ad82f3f9f3c9baa4bfa7
stillgood96 <a787574@naver.com>
2023-09-04 23:14:33 +0900

    단계별풀기 입출력과 사칙연산 9_10430

---
+++ src/main/java/com/study/algorithm/algorithm/baekjoon/chapter1/problem9_10430.java
@@ -0,0 +1,39 @@
+package com.study.algorithm.algorithm.baekjoon.chapter1;
+
+import java.util.Scanner;
+
+public class problem9_10430 {
+
+
+    /**
+     *
+     *(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+     * (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+     * 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+     *
+     *
+     * @Input
+     * 첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+     *
+     *
+     * @Output
+     * 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+     *
+     */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+
+        System.out.println( (a+b)%c);
+        System.out.println( ((a%c) + (b%c))%c );
+        System.out.println( (a*b)%c);
+        System.out.println( ((a%c) * (b%c))%c );
+
+
+    }
+}
