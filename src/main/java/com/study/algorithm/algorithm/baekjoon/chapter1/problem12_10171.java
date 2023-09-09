9666e152939b55c9e965aafdbddb5896bb07e31b
stillgood96 <a787574@naver.com>
2023-09-09 15:39:36 +0900

    단계별풀기 입출력과 사칙연산 12_10171

---
+++ src/main/java/com/study/algorithm/algorithm/baekjoon/chapter1/problem12_10171.java
@@ -0,0 +1,35 @@
+package com.study.algorithm.algorithm.baekjoon.chapter1;
+
+public class problem12_10171 {
+
+    /**
+     *
+     * 아래 예제와 같이 고양이를 출력하시오.
+     *
+     *
+     * \    /\
+     *  )  ( ')
+     * (  /  )
+     *  \(__)|
+     */
+
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer();
+
+
+        sb.append("\\    /\\");
+        sb.append("\n");
+        sb.append(" )  ( ')");
+        sb.append("\n");
+        sb.append("(  /  )");
+        sb.append("\n");
+        sb.append(" \\(__)|");
+
+
+        System.out.println(sb.toString());
+
+
+
+    }
+}
