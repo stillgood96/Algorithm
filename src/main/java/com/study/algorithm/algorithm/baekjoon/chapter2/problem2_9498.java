c8c56e898338395a2fbcd6460d8c81beef33339e
stillgood96 <a787574@naver.com>
2023-09-13 23:03:02 +0900

    단계별풀기 조건문 2_9498

---
+++ src/main/java/com/study/algorithm/algorithm/baekjoon/chapter2/problem2_9498.java
@@ -0,0 +1,54 @@
+package com.study.algorithm.algorithm.baekjoon.chapter2;
+
+import java.util.Scanner;
+
+public class problem2_9498 {
+
+
+  /**
+   *
+   *  시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+   *
+   *   @input
+   *
+   *
+   *   첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+   *
+   *   @Output
+   *
+   *
+   *   시험 성적을 출력한다.
+   */
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int score = sc.nextInt();
+
+    if(score >= 90 && score <=100) {
+
+      System.out.println("A");
+
+    }else if(score >= 80 && score <= 89) {
+
+      System.out.println("B");
+
+    }else if(score >= 70 && score <= 79) {
+
+      System.out.println("C");
+
+    }else if(score >= 60 && score <= 69) {
+
+      System.out.println("D");
+
+    }else {
+
+      System.out.println("F");
+
+    }
+
+
+
+
+  }
+}
